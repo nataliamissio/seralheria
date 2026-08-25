ea522d67ddab122825d34343bdab9890e7bc43f8
Natalia <nataalia.missio@gmail.com>
2026-08-25 17:29:17 -0300

    chore: atualizar galeria e limpar imagens nao usadas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,51 +20,51 @@ export class HomeComponent {
   readonly galleryImages = [
     {
       src: '/assets/img/galeria/trabalho5.webp',
-      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 5'
-    },
-    {
-      src: '/assets/img/galeria/trabalho1.webp',
       alt: 'Projeto de serralheria em estrutura metálica - Trabalho 1'
     },
     {
-      src: '/assets/img/galeria/trabalho11.webp',
-      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 11'
-    },
-    {
-      src: '/assets/img/galeria/trabalho6.webp',
-      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 6'
-    },
-    {
-      src: '/assets/img/galeria/trabalho10.webp',
-      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 10'
-    },
-    {
-      src: '/assets/img/galeria/trabalho3.webp',
-      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 3'
-    },
-    {
-      src: '/assets/img/galeria/trabalho8.webp',
-      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 8'
-    },
-    {
-      src: '/assets/img/galeria/trabalho2.webp',
+      src: '/assets/img/galeria/trabalho1.webp',
       alt: 'Projeto de serralheria em estrutura metálica - Trabalho 2'
     },
     {
-      src: '/assets/img/galeria/trabalho7.webp',
+      src: '/assets/img/galeria/trabalho11.webp',
+      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 3'
+    },
+    {
+      src: '/assets/img/galeria/trabalho6.webp',
+      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 4'
+    },
+    {
+      src: '/assets/img/galeria/trabalho10.webp',
+      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 5'
+    },
+    {
+      src: '/assets/img/galeria/trabalho3.webp',
+      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 6'
+    },
+    {
+      src: '/assets/img/galeria/trabalho8.webp',
       alt: 'Projeto de serralheria em estrutura metálica - Trabalho 7'
     },
     {
-      src: '/assets/img/galeria/trabalho12.webp',
-      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 12'
+      src: '/assets/img/galeria/trabalho2.webp',
+      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 8'
     },
     {
-      src: '/assets/img/galeria/trabalho9.webp',
+      src: '/assets/img/galeria/trabalho7.webp',
       alt: 'Projeto de serralheria em estrutura metálica - Trabalho 9'
     },
     {
+      src: '/assets/img/galeria/trabalho12.webp',
+      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 10'
+    },
+    {
+      src: '/assets/img/galeria/trabalho9.webp',
+      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 11'
+    },
+    {
       src: '/assets/img/galeria/trabalho4.webp',
-      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 4'
+      alt: 'Projeto de serralheria em estrutura metálica - Trabalho 12'
     }
   ];
 
